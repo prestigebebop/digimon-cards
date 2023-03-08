a7fcb4db46754f55a431dc04205e3b508535f6ce
Ben <git@github.com>
2023-03-07 20:07:57 -0800

    remove old comments from previous version using class components

--- src/index.js
+++ src/index.js
@@ -9,16 +9,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* Step 1: App is passed to root.render() => React calls constructor of App component */}
-    {/* Step 2: React calls the App component's render() method. This is how React knows what to show on the screen. Screen will show our h1, SearchBox + DigimonCardList + DigimonCard components*/}
-    {/* Step 3: React calls componentDidMount() lifecycle method to pull API data */}
-    {/* Step 4: After data is retrieved, React changes state of initial empty digimons array with data of array of digimon objects */}
-    {/* Step 5: Since state in React feeds props to children, our individual DigimonCardList and DigimonCard components have access to digimons array data. DigimonCardList can now accept digimons as props */}
     <div className='tc'>
       <App />
     </div>
-    
-    {/* <DigimonCardList digimons={digimons}/> */}
   </React.StrictMode>
 );
 
